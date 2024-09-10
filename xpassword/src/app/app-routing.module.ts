b21041a08d34de17ec401b878c3d5fbfe02e645b
Swellshinider <eduardoleal.contact@gmail.com>
2024-09-10 18:00:55 -0300

    basic demo landing page with routing

--- xpassword/src/app/app-routing.module.ts
+++ xpassword/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NotfoundComponent } from './components/notfound/notfound.component';
+import { LandingComponent } from './components/landing/landing.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: LandingComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '**', component: NotfoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
