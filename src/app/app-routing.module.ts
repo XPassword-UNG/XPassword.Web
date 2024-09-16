938870dbd348b73e025f0e1c63a22822b68b44e8
Swellshinider <eduardoleal.contact@gmail.com>
2024-09-16 09:19:04 -0300

    config

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { NotfoundComponent } from './components/notfound/notfound.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 import { LandingComponent } from './components/landing/landing.component';
+import { LoginComponent } from './components/auth/login/login.component';
+import { SignupComponent } from './components/auth/signup/signup.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { AuthGuard } from './components/auth/auth.guard';
 
 const routes: Routes = [
-  { path: 'home', component: LandingComponent },
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: '**', component: NotfoundComponent }
+  { path: '', component: LandingComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent },
+  { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard] },
+  
+  { path: '**', component: NotFoundComponent },
 ];
 
 @NgModule({
